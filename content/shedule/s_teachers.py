900b04dd36c38da3237ca71343943b1e46769e64
Ilya Pavlov <spirkaa@gmail.com>
2015-12-22 14:42:24 +0300

    split wall of code to functions

--- content/shedule/s_teachers.py
+++ content/shedule/s_teachers.py
@@ -5,50 +5,58 @@ from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 brew = '/opt/homebrew-cask/Caskroom'
-ff = brew + '/firefox/41.0.1/Firefox.app/Contents/MacOS/firefox-bin'
-
-binary = FirefoxBinary(ff)
-driver = webdriver.Firefox(firefox_binary=binary)
+ff = '/firefox/41.0.1/Firefox.app/Contents/MacOS/firefox-bin'
+path = '{}{}'.format(brew, ff)
+driver = webdriver.Firefox(firefox_binary=FirefoxBinary(path))
 
 url = 'https://mrko.mos.ru/dnevnik/'
-driver.get(url)
-
-driver.find_element_by_id('login').send_keys('login')
-driver.find_element_by_id('pass').send_keys('password')
-driver.find_element_by_xpath('//*[@id="em_enter"]/input[4]').click()
-sleep(1)
-driver.get(url + 'services/rasp.php?m=4&day=1')
-sleep(1)
-soup = BeautifulSoup(driver.page_source, 'lxml')
-shedule_table = soup.select_one('table[id=busynessTable] tbody')
-links = shedule_table.select('tr a[href^=javascript]')
-teacher_links = [link.attrs.get('href').split("'")[1] for link in links]
 
 
-def table_to_dict(lessons, daynum):
-    days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
-    result = {days[daynum]: {i+1: lesson[daynum]
-                             for i, lesson
-                             in enumerate(lessons)}}
-    return result
-
-
-shedule = {}
-for link in teacher_links:
-    driver.get(url + 'services/' + link)
-    sleep(0.5)
+def collect_links():
+    driver.get(url)
+    driver.find_element_by_id('login').send_keys('login')
+    driver.find_element_by_id('pass').send_keys('password')
+    driver.find_element_by_xpath('//*[@id="em_enter"]/input[4]').click()
+    sleep(1)
+    driver.get(url + 'services/rasp.php?m=4&day=1')
+    sleep(1)
     soup = BeautifulSoup(driver.page_source, 'lxml')
-    name = soup.select_one('p').get_text().replace('Учитель: ', '')
-    rows = soup.select('tbody tr')[1:]
-    lessons = [[col.text for col in row.select('td')] for row in rows]
-    rasp = [table_to_dict(lessons, x) for x in range(1, 7)]
-    raspdict = {k: v
-                for day in rasp
-                for k, v in day.items()}
-    shedule[name] = raspdict
+    shedule_table = soup.select_one('table[id=busynessTable] tbody')
+    links = shedule_table.select('tr a[href^=javascript]')
+    return [link.attrs.get('href').split("'")[1] for link in links]
 
 
-with open('s_teachers.json', 'w') as outfile:
-    ujson.dump(shedule, outfile)
+def table_to_dict(table):
+    rasp = {}
+    daynames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+    wknd = ['sat', 'sun']
+    for index, day in enumerate(zip(*table)):
+        daydict = {}
+        for i, lesson in enumerate(day):
+            daydict[i+1] = lesson
+        dayname = daynames[index]
+        rasp[dayname] = daydict
+    for k in wknd:
+        rasp.pop(k, None)
+    return rasp
 
-driver.quit()
+
+def main():
+    shedule = {}
+    links = collect_links()
+    for link in links:
+        driver.get(url + 'services/' + link)
+        sleep(0.5)
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        name = soup.select_one('p').get_text().replace('Учитель: ', '')
+        rows = soup.select('tbody tr')[1:]
+        table = [[col.text for col in row.select('td')] for row in rows]
+        shedule[name] = table_to_dict(table)
+    driver.quit()
+
+    with open('s_teachers.json', 'w') as outfile:
+        ujson.dump(shedule, outfile)
+
+
+if __name__ == '__main__':
+    main()
